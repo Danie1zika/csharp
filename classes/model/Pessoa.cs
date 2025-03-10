ccb708797b044afe6dbc208d224ca2b00c78e510
Silas Rafael <161323380+Danie1zika@users.noreply.github.com>
2025-03-10 07:55:59 -0400

    adicionada a classe pessoa e atributos

---
+++ classes/model/Pessoa.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace classes.model
+{
+    public class Pessoa
+    {
+        // Atributos
+        string nome;
+        int idade;
+        double altura;
+        double peso;
+
+        // Construtor
+        public Pessoa(string nome, int idade, double altura, double peso)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.altura = altura;
+            this.peso = peso;
+        }
+    }
+}
